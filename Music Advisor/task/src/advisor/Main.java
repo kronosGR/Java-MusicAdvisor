2b6291d8a95e6b9ec80196db7be354a3ac69c4fe
KronosGR <geo.elgeo@gmail.com>
2023-01-22 11:23:19 +0100

    stage 2/5 completed

--- Music Advisor/task/src/advisor/Main.java
+++ Music Advisor/task/src/advisor/Main.java
@@ -1,9 +1,14 @@
 package advisor;
 
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Main {
     private static Scanner sc = new Scanner(System.in);
+    private static String AUTH_URL = "https://accounts.spotify.com/authorize?client_id=afc222d2075c4e5083c715d5817967e4&redirect_uri=http://localhost:8080&response_type=code";
+
+    private static boolean isAuthenticated = false;
+
 
     public static void main(String[] args) {
 
@@ -15,32 +20,13 @@ public class Main {
 
             switch (choice) {
                 case "new":
-                    System.out.println("---NEW RELEASES---");
-                    System.out.println("Mountains [Sia, Diplo, Labrinth]");
-                    System.out.println("Runaway [Lil Peep]");
-                    System.out.println("The Greatest Show [Panic! At The Disco]");
-                    System.out.println("All Out Life [Slipknot]");
+                    newEndpoint();
                     break;
                 case "featured":
-                    System.out.println("---FEATURED---");
-                    System.out.println("Mellow Morning");
-                    System.out.println("Wake Up and Smell the Coffee");
-                    System.out.println("Monday Motivation");
-                    System.out.println("Songs to Sing in the Shower");
+                    featuredEndpoint();
                     break;
-                case "categories":
-                    System.out.println("---CATEGORIES---");
-                    System.out.println("Top Lists");
-                    System.out.println("Pop");
-                    System.out.println("Mood");
-                    System.out.println("Latin");
-                    break;
-                case "playlists Mood":
-                    System.out.println("---MOOD PLAYLISTS---");
-                    System.out.println("Walk Like A Badass");
-                    System.out.println("Rage Beats");
-                    System.out.println("Arab Mood Booster");
-                    System.out.println("Sunday Stroll");
+                case "auth":
+                    authEndpoint();
                     break;
                 case "exit":
                     active = false;
@@ -48,5 +34,34 @@ public class Main {
                     break;
             }
         }
+    }
+    private static void newEndpoint(){
+        if (!isAuthenticated){
+            System.out.println("Please, provide access for application.");
+            return;
+        };
+        System.out.println("---NEW RELEASES---");
+        System.out.println("Mountains [Sia, Diplo, Labrinth]");
+        System.out.println("Runaway [Lil Peep]");
+        System.out.println("The Greatest Show [Panic! At The Disco]");
+        System.out.println("All Out Life [Slipknot]");
+    }
+
+    private static void featuredEndpoint(){
+        if (!isAuthenticated) {
+            System.out.println("Please, provide access for application.");
+            return;
+        };
+        System.out.println("---FEATURED---");
+        System.out.println("Mellow Morning");
+        System.out.println("Wake Up and Smell the Coffee");
+        System.out.println("Monday Motivation");
+        System.out.println("Songs to Sing in the Shower");
+    }
+
+    private static void authEndpoint(){
+        System.out.println(AUTH_URL);
+        System.out.println("---SUCCESS---");
+        isAuthenticated = true;
     }
 }
